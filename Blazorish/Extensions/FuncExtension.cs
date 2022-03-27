c101383ad6a1238bb073f3bd3242396e07133151
Maxumka <maxumka1792@gmail.com>
2022-03-27 11:08:13 +0700

    Add extension for Func

---
+++ Blazorish/Extensions/FuncExtension.cs
@@ -0,0 +1,9 @@
+﻿namespace Blazorish.Extensions;
+
+public static class FuncExtension
+{
+    public static Func<Exception, T> AsFunc<TDerived, T>(this Func<Exception, TDerived> func)
+        where TDerived : T
+        where T : class 
+        => ex => func(ex);
+}
