a7a1ffd0b0e4cab87a4d4bcf91a2ea556dd618b0
Maxumka <maxumka1792@gmail.com>
2022-03-27 19:03:40 +0700

    Add some new cmd

--- Blazorish/Cmd.cs
+++ Blazorish/Cmd.cs
@@ -7,12 +7,15 @@ public abstract record Cmd<TMsg> where TMsg : class
     public static None<TMsg> None() => 
         new();
 
+    public static OfMsg<TMsg> OfMsg(TMsg msg)
+        => new OfMsg<TMsg>(msg);
+    
     public static OfAsync<TMsg> OfAsync<T>(Func<T, Task> func, T arg)
     {
         return new OfAsync<TMsg>(func(arg));
     }
 
-    public static OfAsyncResult<TMsg> OfAsyncResult<TMsgDerived, T, U>(Func<T, Task<U>> func, T arg, Func<U, TMsgDerived> msg)
+    public static OfAsyncPerform<TMsg> OfAsyncPerform<TMsgDerived, T, U>(Func<T, Task<U>> func, T arg, Func<U, TMsgDerived> msg)
         where TMsgDerived : TMsg
     {
         var msgTask = Task.Run(async () => msg(await func(arg)))
@@ -20,13 +23,34 @@ public abstract record Cmd<TMsg> where TMsg : class
 
         return new(msgTask);
     }
+
+    public static OfAsyncEither<TMsg> OfAsyncEither<TMsgDerived, T, U>(
+        Func<T, Task<U>> func,
+        T arg,
+        Func<U, TMsgDerived> suc,
+        Func<Exception, TMsgDerived> err)
+        where TMsgDerived : TMsg
+    {
+        var sucMsgTask = Task.Run(async () => suc(await func(arg)))
+            .AsTask<TMsgDerived, TMsg>();
+
+        var errMsg = err.AsFunc<TMsgDerived, TMsg>();
+
+        return new OfAsyncEither<TMsg>(sucMsgTask, errMsg);
+    }
 }
 
 public sealed record None<TMsg> : Cmd<TMsg> 
     where TMsg : class;
 
+public sealed record OfMsg<TMsg>(TMsg Msg) : Cmd<TMsg>
+    where TMsg : class;
+
 public sealed record OfAsync<TMsg>(Task Task) : Cmd<TMsg>
     where TMsg : class;
 
-public sealed record OfAsyncResult<TMsg>(Task<TMsg> Msg) : Cmd<TMsg> 
+public sealed record OfAsyncPerform<TMsg>(Task<TMsg> Msg) : Cmd<TMsg> 
     where TMsg : class;
+
+public sealed record OfAsyncEither<TMsg>(Task<TMsg> Suc, Func<Exception, TMsg> Err) : Cmd<TMsg>
+    where TMsg : class;    
